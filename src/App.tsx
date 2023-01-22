719c1b1989580d1f90d5aad24ea2d0979a61401c
Michalis Tripolitsiotis <michalistripolitsiotis@gmail.com>
2023-01-22 12:13:45 +0200

    OE-10: Rename to register

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Layouts/Navbar';
 import Login from './components/authentication/pages/Login';
-import Signup from './components/authentication/pages/Signup';
+import Signup from './components/authentication/pages/Register';
 import Home from './components/home/Home';
 import Dashboard from './components/authentication/pages/authenticated/Dashboard';
 import ProtectedRoute from './components/Layouts/ProtectedRoute';
@@ -18,7 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/register" element={<Signup />} />
         <Route element={<ProtectedRoute token={token} />}>
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="me" element={<Me />} />
