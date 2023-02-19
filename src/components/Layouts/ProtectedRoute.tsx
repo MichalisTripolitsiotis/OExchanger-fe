65a5333dbe94ed1b5686dafe4e839939a7bb5574
Michalis Tripolitsiotis <michalistripolitsiotis@gmail.com>
2023-02-19 12:13:29 +0200

    OE-18: Remove action from protected route

--- src/components/Layouts/ProtectedRoute.tsx
+++ src/components/Layouts/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = ({ token, authenticated, action, redirectPath = '/login', children }) => {
+const ProtectedRoute = ({ token, authenticated, redirectPath = '/login', children }) => {
     if (!token || !authenticated) {
         return <Navigate to={redirectPath} replace />;
     }
